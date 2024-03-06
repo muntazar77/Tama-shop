86132c32410c2eef299f3864ba46fc60fa56376a
Muntadher <127090047+muntazar77@users.noreply.github.com>
2024-03-06 21:48:38 +0300

    Update footer.php

--- admin/include/footer.php
+++ admin/include/footer.php
@@ -1,7 +1,13 @@
   <!-- Argon Scripts -->
   <!-- Core -->
-  <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script>
+<!--   <script src="assets/vendor/jquery/dist/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/dist/js/bootstrap.bundle.min.js"></script> -->
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+
   <!-- <script src="assets/vendor/js-cookie/js.cookie.js"></script> -->
   <!-- <script src="assets/vendor/jquery.scrollbar/jquery.scrollbar.min.js"></script> -->
   <!-- <script src="assets/vendor/jquery-scroll-lock/dist/jquery-scrollLock.min.js"></script> -->
