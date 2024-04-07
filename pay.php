1f29a8bae93dcfbb043bc7f70fa81508adbebf79
Muntadher <127090047+muntazar77@users.noreply.github.com>
2024-04-07 23:31:32 +0300

    Update pay.php

--- pay.php
+++ pay.php
@@ -5,10 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <title>Tama Shop</title>
-<!--     <link
-      rel="stylesheet"
-      href="css/fontawesome-free-5.15.3-web/css/all.min.css"
-    /> -->
+
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.5.1/css/fontawesome.min.css">
     <!-- <link rel="stylesheet" href="css/style.css" /> -->
     <link rel="stylesheet" href="css/pay.css" />
